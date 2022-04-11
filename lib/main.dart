526c360c7e6012c85e6236f6cbee1bc74dc5f1f0
tgibbons-css <tgibbons@css.edu>
2022-04-11 13:05:59 -0500

    Trying to install firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'src/widgets.dart';
+import 'dart:async';                                    // new
+import 'package:cloud_firestore/cloud_firestore.dart';  // new
 
 void main() {
   runApp(App());
